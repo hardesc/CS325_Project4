efa2dd1e6060c474943958a0c83ecc290726e488
Charles Hardes <hardesc@onid.oregonstate.edu>
2015-12-02 14:00:05 -0800

    Initial Commit

---
+++ CS325_Project4/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  CS325_Project4
+//
+//  Created by Charles Hardes on 12/2/15.
+//  Copyright (c) 2015 Charles Hardes. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
